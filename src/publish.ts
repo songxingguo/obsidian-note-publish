f51e112122ab5e3843d67c353675110d5f294843
songxingguo <xg.song@qq.com>
2024-07-02 09:23:30 +0800

    refactor: 格式化

--- src/publish.ts
+++ src/publish.ts
@@ -1,103 +1,109 @@
-import {
-    Notice,
-    Plugin,
-} from "obsidian";
+import { Notice, Plugin } from "obsidian";
 
-import ImageTagProcessor, {ACTION_PUBLISH} from "./uploader/imageTagProcessor";
+import ImageTagProcessor, {
+  ACTION_PUBLISH,
+} from "./uploader/imageTagProcessor";
 import ImageUploader from "./uploader/imageUploader";
-import {ImgurAnonymousSetting} from "./uploader/imgur/imgurAnonymousUploader";
-import {IMGUR_PLUGIN_CLIENT_ID} from "./uploader/imgur/constants";
+import { ImgurAnonymousSetting } from "./uploader/imgur/imgurAnonymousUploader";
+import { IMGUR_PLUGIN_CLIENT_ID } from "./uploader/imgur/constants";
 import ImageStore from "./imageStore";
 import buildUploader from "./uploader/imageUploaderBuilder";
 import PublishSettingTab from "./ui/publishSettingTab";
-import {OssSetting} from "./uploader/oss/ossUploader";
-import {ImagekitSetting} from "./uploader/imagekit/imagekitUploader";
+import { OssSetting } from "./uploader/oss/ossUploader";
+import { ImagekitSetting } from "./uploader/imagekit/imagekitUploader";
 
 export interface PublishSettings {
-    imageAltText: boolean;
-    replaceOriginalDoc: boolean;
-    ignoreProperties: boolean;
-    attachmentLocation: string;
-    imageStore: string;
-    //Imgur Anonymous setting
-    imgurAnonymousSetting: ImgurAnonymousSetting;
-    ossSetting: OssSetting;
-    imagekitSetting: ImagekitSetting;
+  imageAltText: boolean;
+  replaceOriginalDoc: boolean;
+  ignoreProperties: boolean;
+  attachmentLocation: string;
+  imageStore: string;
+  //Imgur Anonymous setting
+  imgurAnonymousSetting: ImgurAnonymousSetting;
+  ossSetting: OssSetting;
+  imagekitSetting: ImagekitSetting;
 }
 
 const DEFAULT_SETTINGS: PublishSettings = {
-    imageAltText: true,
-    replaceOriginalDoc: false,
-    ignoreProperties: true,
-    attachmentLocation: ".",
-    imageStore: ImageStore.IMGUR.id,
-    imgurAnonymousSetting: {clientId: IMGUR_PLUGIN_CLIENT_ID},
-    ossSetting: {
-        region: "oss-cn-hangzhou",
-        accessKeyId: "",
-        accessKeySecret: "",
-        bucket: "",
-        endpoint: "https://oss-cn-hangzhou.aliyuncs.com/",
-        path: "",
-        customDomainName: "",
-    },
-    imagekitSetting: {
-        endpoint: "",
-        imagekitID: "",
-        privateKey: "",
-        publicKey: "",
-        folder: "",
-    }
+  imageAltText: true,
+  replaceOriginalDoc: false,
+  ignoreProperties: true,
+  attachmentLocation: ".",
+  imageStore: ImageStore.IMGUR.id,
+  imgurAnonymousSetting: { clientId: IMGUR_PLUGIN_CLIENT_ID },
+  ossSetting: {
+    region: "oss-cn-hangzhou",
+    accessKeyId: "",
+    accessKeySecret: "",
+    bucket: "",
+    endpoint: "https://oss-cn-hangzhou.aliyuncs.com/",
+    path: "",
+    customDomainName: "",
+  },
+  imagekitSetting: {
+    endpoint: "",
+    imagekitID: "",
+    privateKey: "",
+    publicKey: "",
+    folder: "",
+  },
 };
 export default class ObsidianPublish extends Plugin {
-    settings: PublishSettings;
-    imageTagProcessor: ImageTagProcessor;
-    imageUploader: ImageUploader;
+  settings: PublishSettings;
+  imageTagProcessor: ImageTagProcessor;
+  imageUploader: ImageUploader;
 
-    async onload() {
-        await this.loadSettings();
-        this.setupImageUploader()
-        this.addStatusBarItem().setText("Status Bar Text");
-        this.addCommand({
-            id: "publish-page",
-            name: "Publish Page",
-            checkCallback: (checking: boolean) => {
-                if (!checking) {
-                    this.publish()
-                }
-                return true;
-            }
-        });
-        this.addSettingTab(new PublishSettingTab(this.app, this));
-    }
-
-    onunload() {
-        // console.log("unloading plugin");
-    }
-
-    async loadSettings() {
-        this.settings = Object.assign({}, DEFAULT_SETTINGS, (await this.loadData()) as PublishSettings);
-    }
-
-    async saveSettings() {
-        await this.saveData(this.settings);
-    }
-
-    private publish(): void {
-        if (!this.imageUploader) {
-            new Notice("Image uploader setup failed, please check setting.")
-        } else {
-            this.imageTagProcessor.process(ACTION_PUBLISH).then(() => {
-            });
+  async onload() {
+    await this.loadSettings();
+    this.setupImageUploader();
+    this.addStatusBarItem().setText("Status Bar Text");
+    this.addCommand({
+      id: "publish-page",
+      name: "Publish Page",
+      checkCallback: (checking: boolean) => {
+        if (!checking) {
+          this.publish();
         }
-    }
+        return true;
+      },
+    });
+    this.addSettingTab(new PublishSettingTab(this.app, this));
+  }
 
-    setupImageUploader(): void {
-        try {
-            this.imageUploader = buildUploader(this.settings);
-            this.imageTagProcessor = new ImageTagProcessor(this.app, this.settings, this.imageUploader);
-        } catch (e) {
-            console.log(`Failed to setup image uploader: ${e}`)
-        }
+  onunload() {
+    // console.log("unloading plugin");
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign(
+      {},
+      DEFAULT_SETTINGS,
+      (await this.loadData()) as PublishSettings
+    );
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+
+  private publish(): void {
+    if (!this.imageUploader) {
+      new Notice("Image uploader setup failed, please check setting.");
+    } else {
+      this.imageTagProcessor.process(ACTION_PUBLISH).then(() => {});
     }
+  }
+
+  setupImageUploader(): void {
+    try {
+      this.imageUploader = buildUploader(this.settings);
+      this.imageTagProcessor = new ImageTagProcessor(
+        this.app,
+        this.settings,
+        this.imageUploader
+      );
+    } catch (e) {
+      console.log(`Failed to setup image uploader: ${e}`);
+    }
+  }
 }
